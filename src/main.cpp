e2d5fa346d82dbee171052600f59da3c142199aa
DonPavlov <1541649+DonPavlov@users.noreply.github.com>
2021-02-28 15:43:48 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Wifi.h>
+#include <WiFiUdp.h>
+#include <ESPmDNS.h>
+#include "TelnetSpy.cpp"
+#include "secrets.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
